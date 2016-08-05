b415e1e086f250bcc25571e85f5635a24bbc70b0
Xavier Defrang <xavier.defrang@gmail.com>
2016-08-05 19:59:58 +0200

    Simplify last key lookup

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Tributary.Mixfile do
 
   def project do
     [app: :tributary,
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
