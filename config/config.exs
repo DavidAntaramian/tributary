dde1478737cfffe81df534353514f21aa33333bf
David Antaramian <david@antaramian.com>
2016-08-10 02:46:57 -0400

    Rearrange configuration to use private configs

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-import_config "#{Mix.env}.exs"
+if File.exists?("config/#{Mix.env}.exs") do
+  import_config "#{Mix.env}.exs"
+end
+
+if File.exists?("config/#{Mix.env}.secret.exs") do
+  import_config "#{Mix.env}.secret.exs"
+end
